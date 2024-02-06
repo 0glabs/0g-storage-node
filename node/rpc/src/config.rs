e9045e7b65e4a1a2d1cac4101f2f3996144e6adb
MiniFrenchBread <103425574+MiniFrenchBread@users.noreply.github.com>
2024-02-06 16:37:04 +0800

    feat: max request body

--- node/rpc/src/config.rs
+++ node/rpc/src/config.rs
@@ -6,5 +6,6 @@ pub struct Config {
     pub listen_address: SocketAddr,
     pub listen_address_admin: Option<SocketAddr>,
     pub chunks_per_segment: usize,
+    pub max_request_body_size: u32,
     pub max_cache_file_size: usize,
 }
