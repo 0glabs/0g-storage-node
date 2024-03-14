31846345b2c846f85faac547c234231d0fddd05c
Chenxing Li <ChenxingLi@users.noreply.github.com>
2024-03-14 12:43:26 +0800

    Fix compile

--- common/contract-interface/build.rs
+++ common/contract-interface/build.rs
@@ -5,26 +5,31 @@ const INSTALL_ERROR_MESSAGE: &str =
 const COMPILE_ERROR_MESSAGE: &str =
     "Compile solidity contracts fail, try to run `yarn compile` in folder '0g-storage-contracts'";
 
+fn compile_contracts() {
+    println!("cargo:rerun-if-changed=../../0g-storage-contracts/contracts/");
+    println!("cargo:rerun-if-changed=../../0g-storage-contracts/hardhat.config.ts");
+
+    let output = Command::new("yarn")
+        .arg("--cwd")
+        .arg("../../0g-storage-contracts")
+        .status()
+        .expect(INSTALL_ERROR_MESSAGE);
+    assert!(output.success(), "{}", INSTALL_ERROR_MESSAGE);
+
+    let output = Command::new("yarn")
+        .arg("--cwd")
+        .arg("../../0g-storage-contracts")
+        .arg("compile")
+        .status()
+        .expect(COMPILE_ERROR_MESSAGE);
+    assert!(output.success(), "{}", COMPILE_ERROR_MESSAGE);
+}
+
 fn main() {
-    if cfg!(feature = "compile-contracts") {
-        println!("cargo:rerun-if-changed=../../0g-storage-contracts/contracts/");
-        println!("cargo:rerun-if-changed=../../0g-storage-contracts/hardhat.config.ts");
-
-        let output = Command::new("yarn")
-            .arg("--cwd")
-            .arg("../../0g-storage-contracts")
-            .status()
-            .expect(INSTALL_ERROR_MESSAGE);
-        assert!(output.success(), "{}", INSTALL_ERROR_MESSAGE);
-
-        let output = Command::new("yarn")
-            .arg("--cwd")
-            .arg("../../0g-storage-contracts")
-            .arg("compile")
-            .status()
-            .expect(COMPILE_ERROR_MESSAGE);
-        assert!(output.success(), "{}", COMPILE_ERROR_MESSAGE);
-    } else {
-        println!("cargo:rerun-if-changed=../../0g-storage-contracts/artifacts/");
+    if false /*  Disable build contracts on rust compilation */ && cfg!(feature = "compile-contracts")
+    {
+        compile_contracts();
+        return;
     }
+    println!("cargo:rerun-if-changed=../../0g-storage-contracts/artifacts/");
 }
