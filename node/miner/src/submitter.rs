cbf1db8907797e20c978b3ae4dd17a35f33835b0
Peilun Li <peilun.li@confluxnetwork.org>
2024-07-08 11:37:33 +0800

    Add zgs_getSectorProof.

--- node/miner/src/submitter.rs
+++ node/miner/src/submitter.rs
@@ -81,7 +81,7 @@ impl Submitter {
         let flow_proof = self
             .store
             .get_proof_at_root(
-                mine_answer.context_flow_root,
+                Some(mine_answer.context_flow_root),
                 mine_answer.recall_position,
                 SECTORS_PER_SEAL as u64,
             )
