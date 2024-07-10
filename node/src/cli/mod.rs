0fceb7b640fe65701b3960778e5d75f652871e6b
boqiu <82121246@qq.com>
2024-07-10 12:18:00 +0800

    Add cli args

--- node/src/cli/mod.rs
+++ node/src/cli/mod.rs
@@ -3,5 +3,8 @@ use clap::{arg, command, Command};
 pub fn cli_app<'a>() -> Command<'a> {
     command!()
         .arg(arg!(-c --config <FILE> "Sets a custom config file"))
+        .arg(arg!(--"miner-key" [KEY] "Sets miner private key"))
+        .arg(arg!(--"blockchain-rpc-endpoint" [URL] "Sets blockchain RPC endpoint"))
+        .arg(arg!(--"db-max-num-chunks" [NUM] "Sets the max number of chunks to store in db"))
         .allow_external_subcommands(true)
 }
