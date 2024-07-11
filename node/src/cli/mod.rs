59d3a03487df675f0c321ac450223b89ab6927d6
Bo QIU <35757521+boqiu@users.noreply.github.com>
2024-07-11 11:54:13 +0800

    Hotfix for cli default value (#118)

--- node/src/cli/mod.rs
+++ node/src/cli/mod.rs
@@ -3,11 +3,10 @@ use clap::{arg, command, Command};
 pub fn cli_app<'a>() -> Command<'a> {
     command!()
         .arg(arg!(-c --config <FILE> "Sets a custom config file"))
-        .arg(arg!(--"miner-key" [KEY] "Sets miner private key"))
+        .arg(arg!(--"miner-key" [KEY] "Sets miner private key (Default: None)"))
         .arg(
-            arg!(--"blockchain-rpc-endpoint" [URL] "Sets blockchain RPC endpoint")
-                .default_value("http://127.0.0.1:8545"),
+            arg!(--"blockchain-rpc-endpoint" [URL] "Sets blockchain RPC endpoint (Default: http://127.0.0.1:8545)")
         )
-        .arg(arg!(--"db-max-num-chunks" [NUM] "Sets the max number of chunks to store in db"))
+        .arg(arg!(--"db-max-num-chunks" [NUM] "Sets the max number of chunks to store in db (Default: None)"))
         .allow_external_subcommands(true)
 }
