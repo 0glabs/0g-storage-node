a79f7bbf126df3748d072076dccdc7e0c34bd971
bruno-valante <140794260+bruno-valante@users.noreply.github.com>
2024-08-27 18:08:10 +0800

    Fix contract interface build cache (#172)

--- common/contract-interface/build.rs
+++ common/contract-interface/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     if cfg!(not(feature = "dev")) {
-        println!("cargo:rerun-if-changed=../../0g-storage-contracts/artifacts/");
+        println!("cargo:rerun-if-changed=../../storage-contracts-abis/");
     } else {
         println!("cargo:rerun-if-changed=../../0g-storage-contracts-dev/artifacts/");
     }
