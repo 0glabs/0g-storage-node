4dc0102d1d6088782db1fbc31263282fc287b6dd
boqiu <82121246@qq.com>
2024-08-28 19:05:17 +0800

    fix batcher

--- node/router/src/batcher.rs
+++ node/router/src/batcher.rs
@@ -29,36 +29,108 @@ impl<T> Batcher<T> {
     }
 
     pub fn add(&mut self, value: T) -> Option<Vec<T>> {
+        self.add_with_time(value, Instant::now())
+    }
+
+    fn add_with_time(&mut self, value: T, now: Instant) -> Option<Vec<T>> {
         // push at front so as to use `split_off` to remove expired items
-        self.items.push_front((value, Instant::now()));
+        self.items.push_front((value, now));
 
         // remove expired items if not full
         let size = self.items.len();
         if size < self.capacity {
-            return self.expire();
+            return self.expire_with_time(now);
         }
 
         // remove all items for batch operation in advance
         self.metrics_batch_size.update(size as u64);
 
         Some(Vec::from_iter(
-            self.items.split_off(0).into_iter().map(|(val, _)| val),
+            self.items
+                .split_off(0)
+                .into_iter()
+                .rev()
+                .map(|(val, _)| val),
         ))
     }
 
     pub fn expire(&mut self) -> Option<Vec<T>> {
+        self.expire_with_time(Instant::now())
+    }
+
+    fn expire_with_time(&mut self, now: Instant) -> Option<Vec<T>> {
+        let total = self.items.len();
+
         // find the index of first expired item if any
-        let pos = self
+        let first_unexpired = self
             .items
             .iter()
-            .position(|(_, ts)| ts.elapsed() > self.timeout)?;
+            .rev()
+            .position(|(_, ts)| now.duration_since(*ts) < self.timeout)
+            .unwrap_or(total);
+
+        if first_unexpired == 0 {
+            return None;
+        }
+
+        let pos = total - first_unexpired;
 
         // remove expired items for batch operation in advance
-        let size = self.items.len() - pos;
-        self.metrics_batch_size.update(size as u64);
+        self.metrics_batch_size.update((total - pos) as u64);
 
         Some(Vec::from_iter(
-            self.items.split_off(pos).into_iter().map(|(val, _)| val),
+            self.items
+                .split_off(pos)
+                .into_iter()
+                .rev()
+                .map(|(val, _)| val),
         ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::{Duration, Instant};
+
+    use super::Batcher;
+
+    #[test]
+    fn test_add() {
+        let mut batcher: Batcher<usize> = Batcher::new(3, Duration::from_secs(10), "test");
+
+        assert_eq!(batcher.add(1), None);
+        assert_eq!(batcher.add(2), None);
+        assert_eq!(batcher.add(3), Some(vec![1, 2, 3]));
+        assert_eq!(batcher.items.len(), 0);
+    }
+
+    #[test]
+    fn test_expire() {
+        let mut batcher: Batcher<usize> = Batcher::new(5, Duration::from_secs(10), "test");
+
+        let now = Instant::now();
+
+        // enqueue: 1, 2, 3, 4
+        assert_eq!(batcher.add_with_time(1, now + Duration::from_secs(1)), None);
+        assert_eq!(batcher.add_with_time(2, now + Duration::from_secs(2)), None);
+        assert_eq!(batcher.add_with_time(3, now + Duration::from_secs(4)), None);
+        assert_eq!(batcher.add_with_time(4, now + Duration::from_secs(5)), None);
+
+        // expire None
+        assert_eq!(batcher.expire_with_time(now + Duration::from_secs(6)), None);
+
+        // expire 1, 2
+        assert_eq!(
+            batcher.expire_with_time(now + Duration::from_secs(13)),
+            Some(vec![1, 2])
+        );
+        assert_eq!(batcher.items.len(), 2);
+
+        // expire 3, 4
+        assert_eq!(
+            batcher.expire_with_time(now + Duration::from_secs(20)),
+            Some(vec![3, 4])
+        );
+        assert_eq!(batcher.items.len(), 0);
     }
 }
