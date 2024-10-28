8bd2b079e68b86f437432fdf4fc52b4c242b1f94
Peter Zhang <peter@0g.ai>
2024-10-28 23:27:04 +0800

    add detailed metrics for storage layer

--- node/storage/src/log_store/log_manager.rs
+++ node/storage/src/log_store/log_manager.rs
@@ -191,6 +191,7 @@ impl LogStoreChunkWrite for LogManager {
         chunks: ChunkArray,
         maybe_file_proof: Option<FlowProof>,
     ) -> Result<bool> {
+        let start_time = Instant::now();
         let mut merkle = self.merkle.write();
         let tx = self
             .tx_store
@@ -222,6 +223,7 @@ impl LogStoreChunkWrite for LogManager {
                 tx.start_entry_index,
             )?;
         }
+        metrics::PUT_CHUNKS.update_since(start_time);
         Ok(true)
     }
 
