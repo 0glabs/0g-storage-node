3501ed200845c41633cf8ddab5269092865f1080
Peter Zhang <peter@0g.ai>
2024-11-18 18:56:52 +0800

    add snapshot test

--- tests/snapshot_test.py
+++ tests/snapshot_test.py
@@ -9,28 +9,35 @@ class SnapshotTask(TestFramework):
     def setup_params(self):
         self.num_nodes = 2
 
+        # Enable random auto sync only
+        for i in range(self.num_nodes):
+            self.zgs_node_configs[i] = {
+                "sync": {
+                    "auto_sync_enabled": True,
+                    "max_sequential_workers": 3,
+                    "max_random_workers": 3,
+                }
+            }
+
     def run_test(self):
         # Stop the last node to verify historical file sync
 
         # Submit and upload files on node 0
         data_root_1 = self.__upload_file__(0, 256 * 1024)
-        data_root_2 = self.__upload_file__(0, 256 * 1024)
         wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_1) is not None)
         wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_1)["finalized"])
-        wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_2) is not None)
-        wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_2)["finalized"])
 
         # Start the last node to verify historical file sync
         self.nodes[1].shutdown()
-        shutil.rmtree(os.path.join(self.nodes[1].data_dir, 'data_db'))
+        shutil.rmtree(os.path.join(self.nodes[1].data_dir, 'db/data_db'))
         
         self.start_storage_node(1)
         self.nodes[1].wait_for_rpc_connection()
         
-        wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_1) is not None)
-        wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_1)["finalized"])
-        wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_2) is not None)
-        wait_until(lambda: self.nodes[1].zgs_get_file_info(data_root_2)["finalized"])
+        # Submit and upload files on node 0
+        data_root_1 = self.__upload_file__(1, 256 * 1024)
+        wait_until(lambda: self.nodes[0].zgs_get_file_info(data_root_1) is not None)
+        wait_until(lambda: self.nodes[0].zgs_get_file_info(data_root_1)["finalized"])
 
 if __name__ == "__main__":
     SnapshotTask().main()
