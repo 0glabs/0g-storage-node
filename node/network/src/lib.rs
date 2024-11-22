8ca6585bd0b3fa264ed78c4e66478f779372e84a
boqiu <82121246@qq.com>
2024-11-22 17:11:50 +0800

    Upgrade network protocol version

--- node/network/src/lib.rs
+++ node/network/src/lib.rs
@@ -96,8 +96,10 @@ pub use service::{load_private_key, Context, Libp2pEvent, Service, NETWORK_KEY_F
 /// Defines the current P2P protocol version.
 /// - v1: Broadcast FindFile & AnnounceFile messages in the whole network, which caused network too heavey.
 /// - v2: Publish NewFile to neighbors only and announce file via RPC message.
+/// - v3: Add shard config in Status message.
 pub const PROTOCOL_VERSION_V1: [u8; 3] = [0, 1, 1];
 pub const PROTOCOL_VERSION_V2: [u8; 3] = [0, 2, 1];
+pub const PROTOCOL_VERSION_V3: [u8; 3] = [0, 3, 0];
 
 /// Application level requests sent to the network.
 #[derive(Debug, Clone, Copy)]
--- node/src/config/convert.rs
+++ node/src/config/convert.rs
@@ -39,18 +39,17 @@ impl ZgsConfig {
             .await
             .map_err(|e| format!("Unable to get chain id: {:?}", e))?
             .as_u64();
-        let network_protocol_version = if self.sync.neighbors_only {
-            network::PROTOCOL_VERSION_V2
-        } else {
-            network::PROTOCOL_VERSION_V1
-        };
         let local_network_id = NetworkIdentity {
             chain_id,
             flow_address,
             p2p_protocol_version: ProtocolVersion {
-                major: network_protocol_version[0],
-                minor: network_protocol_version[1],
-                build: network_protocol_version[2],
+                major: network::PROTOCOL_VERSION_V3[0],
+                minor: network::PROTOCOL_VERSION_V3[1],
+                build: if self.sync.neighbors_only {
+                    network::PROTOCOL_VERSION_V3[2] + 1
+                } else {
+                    network::PROTOCOL_VERSION_V3[2]
+                },
             },
         };
         network_config.network_id = local_network_id.clone();
