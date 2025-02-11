2272b5dbfd230fc859939be0345cba4b2f1dda29
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-02-11 11:53:28 +0300

    fix: Fix path handling in script execution (#318)

--- scripts/check_abis.sh
+++ scripts/check_abis.sh
@@ -1,12 +1,11 @@
-
 set -e
 
 artifacts_path="$1"
 
 check_abis() {
     for contract_name in "$@"; do
-        diff $(./scripts/search_abi.sh "$artifacts_path" "$contract_name.json") "storage-contracts-abis/$contract_name.json"
+        diff "$(./scripts/search_abi.sh "$artifacts_path" "$contract_name.json")" "storage-contracts-abis/$contract_name.json"
     done
 }
-check_abis DummyMarket DummyReward Flow PoraMine PoraMineTest FixedPrice ChunkLinearReward FixedPriceFlow
 
+check_abis DummyMarket DummyReward Flow PoraMine PoraMineTest FixedPrice ChunkLinearReward FixedPriceFlow
