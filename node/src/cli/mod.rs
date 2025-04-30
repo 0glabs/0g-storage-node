229523ceec1975f965bbc4d4c906b49df7926dc8
0g-peterzhb <158457852+0g-peterzhb@users.noreply.github.com>
2025-04-30 14:50:18 +0800

    update the dockerfile to build the node (#366)

--- node/src/cli/mod.rs
+++ node/src/cli/mod.rs
@@ -9,6 +9,7 @@ pub fn cli_app() -> Command {
             arg!(--"blockchain-rpc-endpoint" [URL] "Sets blockchain RPC endpoint (Default: http://127.0.0.1:8545)")
         )
         .arg(arg!(--"db-max-num-chunks" [NUM] "Sets the max number of chunks to store in db (Default: None)"))
+        .arg(arg!(--"network-enr-address" [URL] "Sets the network ENR address (Default: None)"))
         .allow_external_subcommands(true)
         .version(zgs_version::VERSION)
 }
